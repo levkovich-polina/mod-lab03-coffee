d7461e822f7ad8d62e13bcd4340c1c14a54b211f
Levkovich Polina <32965861+levkovich-polina@users.noreply.github.com>
2024-03-31 16:51:55 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,42 +5,18 @@
 int main() {
     Automata automat = Automata();
     automat.on();
-    std::cout << automat.getState() << std::endl;
+     automat.off();
     automat.coin(150);
-
-    std::cout << automat.getState() << std::endl;
     automat.coin(200);
-
-    std::cout << automat.getState() << std::endl;
     automat.cancel();
-
-    std::cout << automat.getState() << std::endl;
     automat.coin(0);
-
-    std::cout << automat.getState() << std::endl;
     automat.getMenu();
-
     automat.choice(1);
-    std::cout << automat.getState() << std::endl;
-
     automat.check(1);
-    std::cout << automat.getState() << std::endl;
-
     automat.coin(50);
-    std::cout << automat.getState() << std::endl;
-
     automat.choice(2);
-    std::cout << automat.getState() << std::endl;
-
     automat.check(2);
-    std::cout << automat.getState() << std::endl;
-
     automat.cook();
-    std::cout << automat.getState() << std::endl;
-
     automat.finish();
-    std::cout << automat.getState() << std::endl;
 
-    automat.off();
-    std::cout << automat.getState() << std::endl;
 }
