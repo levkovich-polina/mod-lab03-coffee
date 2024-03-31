ab983eba6789929d52675198be03214684648b75
Levkovich Polina <32965861+levkovich-polina@users.noreply.github.com>
2024-03-31 22:57:25 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,44 @@
-
+//Copyright 2024 <levkovich-polina>
+#include <iostream>
+#include <string>
 #include "Automata.h"
 
-
 int main() {
-    Automata A;
-    A.on();
-    A.coin(150);
-    A.choice(3);
-    A.off();
-    return 0;
+    Automata automat = Automata();
+    automat.on();
+    std::cout << automat.getState() << std::endl;
+    automat.coin(150);
+
+    std::cout << automat.getState() << std::endl;
+    automat.coin(200);
+
+    std::cout << automat.getState() << std::endl;
+    automat.cancel();
+
+    std::cout << automat.getState() << std::endl;
+    automat.coin(0);
+
+    std::cout << automat.getState() << std::endl;
+    automat.getMenu();
+
+    automat.choice(1);
+    std::cout << automat.getState() << std::endl;
+
+    automat.check(1);
+    std::cout << automat.getState() << std::endl;
+
+    automat.coin(50);
+    std::cout << automat.getState() << std::endl;
+
+    automat.choice(2);
+    std::cout << automat.getState() << std::endl;
+
+    automat.check(2);
+    std::cout << automat.getState() << std::endl;
+
+    automat.cook();
+    std::cout << automat.getState() << std::endl;
+
+    automat.finish();
+    std::cout << automat.getState() << std::endl;
 }
