7c519c643181d57c0b0dbdece134dcbdda55accc
Levkovich Polina <32965861+levkovich-polina@users.noreply.github.com>
2024-03-31 22:25:57 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,66 +2,54 @@
 
 #include <gtest/gtest.h>
 #include "Automata.h"
-
-TEST(test_create, test_create) {
-    Automata automat;
-    EXPECT_EQ(OFF, automat.getState());
+TEST(Automata, TestOn) {
+  Automata automata;
+  automata.on();
+  EXPECT_EQ(automata.getState(), Automata::WAIT);
+}
+TEST(Automata, TestOff) {
+  Automata automata;
+  automata.on();
+  automata.off();
+  EXPECT_EQ(automata.getState(), Automata::OFF);
+}
+TEST(Automata, TestWait) {
+  Automata automata;
+  automata.on();
+  automata.coin(200);
+  automata.cancel();
+  EXPECT_EQ(automata.getState(), Automata::WAIT);
+}
+TEST(Automata, TestFinish) {
+  Automata automata;
+  automata.on();
+  automata.coin(200);
+  automata.choice(2);
+  EXPECT_EQ(automata.getState(), Automata::WAIT);
 }
 
-TEST(test_on, test_on) {
-    Automata automat;
-    automat.on();
-    EXPECT_EQ(WAIT, automat.getState());
+TEST(Automata, TestMenu) {
+  Automata automata;
+  automata.on();
+  automata.coin(200);
+  automata.choice(10);
+  EXPECT_EQ(automata.getState(), Automata::ACCEPT);
 }
-
-TEST(test_coin, test_coin) {
-    Automata automat;
-    automat.on();
-    automat.coin(150);
-    EXPECT_EQ(ACCEPT, automat.getState());
+TEST(Automata, TestCash) {
+  Automata automata;
+  automata.on();
+  automata.coin(10);
+  automata.choice(2);
+  EXPECT_EQ(automata.getState(), Automata::WAIT);
 }
-
-TEST(test_off, test_off) {
-    Automata automat;
-    automat.on();
-    automat.off();
-    EXPECT_EQ(OFF, automat.getState());
+TEST(Automata, TestCoin) {
+  Automata automata;
+  automata.on();
+  automata.choice(2);
+  EXPECT_EQ(automata.getState(), Automata::WAIT);
 }
-
-TEST(test_choice, test_choice) {
-    Automata automat;
-    automat.on();
-    automat.coin(100);
-    automat.choice(2);
-    EXPECT_EQ(WAIT, automat.getState());
-}
-
-TEST(test_choice_v2, test_choice_v2) {
-    Automata automat;
-    automat.on();
-    automat.coin(10);
-    automat.choice(2);
-    EXPECT_EQ(WAIT, automat.getState());
-}
-
-TEST(test_cook, test_cook) {
-    Automata automat;
-    automat.on();
-    automat.cook();
-    EXPECT_EQ(COOK, automat.getState());
-}
-
-TEST(test_cancel, test_cancel) {
-    Automata automat;
-    automat.cancel();
-    EXPECT_EQ(WAIT, automat.getState());
-}
-
-TEST(test_full_work, test_full_work) {
-    Automata automat;
-    automat.on();
-    automat.coin(10);
-    automat.choice(2);
-    automat.off();
-    EXPECT_EQ(OFF, automat.getState());
+TEST(Automata, TestAccept) {
+  Automata automata;
+  automata.coin(150);
+  EXPECT_EQ(automata.getState(), Automata::OFF);
 }
