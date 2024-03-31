54aebbc4612ac70cb6b7d8ac3362fc9331f33bac
Levkovich Polina <32965861+levkovich-polina@users.noreply.github.com>
2024-03-31 22:25:24 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,11 @@
 #include <iostream>
 #include <string>
 #include "Automata.h"
-
 int main() {
-    Automata automat = Automata();
-    automat.on();
-    std::cout << automat.getState() << std::endl;
-    automat.coin(150);
-
-    std::cout << automat.getState() << std::endl;
-    automat.coin(200);
-
-    std::cout << automat.getState() << std::endl;
-    automat.cancel();
-
-    std::cout << automat.getState() << std::endl;
-    automat.coin(0);
-
-    std::cout << automat.getState() << std::endl;
-    automat.getMenu();
-
-    automat.choice(1);
-    std::cout << automat.getState() << std::endl;
-
-    automat.check(1);
-    std::cout << automat.getState() << std::endl;
-
-    automat.coin(50);
-    std::cout << automat.getState() << std::endl;
-
-    automat.choice(2);
-    std::cout << automat.getState() << std::endl;
-
-    automat.check(2);
-    std::cout << automat.getState() << std::endl;
-
-    automat.cook();
-    std::cout << automat.getState() << std::endl;
-
-    automat.finish();
-    std::cout << automat.getState() << std::endl;
-
-    automat.off();
-    std::cout << automat.getState() << std::endl;
+  setlocale(LC_ALL, "Russian");
+  Automata p;
+  p.getMenu();
+  p.on();
+  p.coin(200);
+  p.choice(2);
 }
